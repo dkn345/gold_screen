52bc1be84d005c60a7a2031fb2cc57ecda973cc1
shaya05 <akshuokc@gmail.com>
2026-05-05 14:08:18 -0500

    edited favicon and admin button

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
 // Database connection pool
 const pool = mysql.createPool({
